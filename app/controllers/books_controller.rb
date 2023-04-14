28cad434b97bc02112fa198963b9d8b0035da29d
tackwan <129726011+tackwan@users.noreply.github.com>
2023-04-14 12:27:42 +0000

    [update] new

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,46 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "投稿が成功しました"
+      redirect_to book_path(@book.id)
+    else
+     render :index
+
+    end
+  end
+
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book =Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(list.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+  # ストロングパラメータ
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
   end
 end
